83d6ec0549c2d93ae09748b554f5b8855f6609ad
Becky Reamy <rlreamy@umich.edu>
2020-09-10 14:16:38 -0400

    KPMP-2108: Added new endpoint to get all slides. Also added LoggingService and Shibboleth since they are connected, and this will let us start logging requests

--- src/main/java/org/kpmp/slides/SlideService.java
+++ src/main/java/org/kpmp/slides/SlideService.java
@@ -9,17 +9,25 @@ import org.springframework.stereotype.Service;
 @Service
 public class SlideService {
 
-	private PatientSlidesRepository patientRepo;
+	private ParticipantRepository participantRepo;
 
 	@Autowired
-	public SlideService(PatientSlidesRepository patientRepo) {
-		this.patientRepo = patientRepo;
+	public SlideService(ParticipantRepository participantRepo) {
+		this.participantRepo = participantRepo;
 	}
 
-	public List<Slide> getSlidesForPatient(String kpmpId) {
-		PatientSlides patient = patientRepo.findByKpmpId(kpmpId);
+	public List<Slide> getSlidesForPaarticipant(String kpmpId) {
+		Participant patient = participantRepo.findByKpmpId(kpmpId);
 		if (patient != null) {
 			return patient.getSlides();
+		}
+		return Collections.emptyList();
+	}
+
+	public List<Participant> getAllParticipants() {
+		List<Participant> participants = participantRepo.findByOrderByKpmpIdAsc();
+		if (participants != null) {
+			return participants;
 		}
 		return Collections.emptyList();
 	}
--- src/test/java/org/kpmp/slides/SlideControllerTest.java
+++ src/test/java/org/kpmp/slides/SlideControllerTest.java
@@ -2,14 +2,18 @@ package org.kpmp.slides;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.Arrays;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.kpmp.logging.LoggingService;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
@@ -18,11 +22,13 @@ public class SlideControllerTest {
 	@Mock
 	private SlideService slideService;
 	private SlideController controller;
+	@Mock
+	private LoggingService logger;
 
 	@Before
 	public void setUp() throws Exception {
 		MockitoAnnotations.initMocks(this);
-		controller = new SlideController(slideService);
+		controller = new SlideController(slideService, logger);
 	}
 
 	@After
@@ -33,10 +39,22 @@ public class SlideControllerTest {
 	@Test
 	public void testGetSlidesForPatient() {
 		List<Slide> slides = Arrays.asList(mock(Slide.class));
-		when(slideService.getSlidesForPatient("444")).thenReturn(slides);
+		when(slideService.getSlidesForPaarticipant("444")).thenReturn(slides);
 
-		List<Slide> result = controller.getSlidesForPatient("444");
+		HttpServletRequest request = mock(HttpServletRequest.class);
+		List<Slide> result = controller.getSlidesForParticipant("444", request);
 
+		verify(logger).logInfoMessage(controller.getClass(), "Getting slides for participant 444", request);
+		assertEquals(slides, result);
+	}
+
+	@Test
+	public void testGetAllSlides() {
+		List<Participant> slides = Arrays.asList(mock(Participant.class));
+		when(slideService.getAllParticipants()).thenReturn(slides);
+		HttpServletRequest request = mock(HttpServletRequest.class);
+		List<Participant> result = controller.getAllParticipants(request);
+		verify(logger).logInfoMessage(controller.getClass(), "Getting all participants", request);
 		assertEquals(slides, result);
 	}
 
