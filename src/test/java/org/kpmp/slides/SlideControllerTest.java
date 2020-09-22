11fca67c34ea7a639e601bd0a0af8b37b915a91c
Becky Reamy <rlreamy@umich.edu>
2020-09-22 11:55:24 -0400

    KPMP-2083: Fix tests

--- src/test/java/org/kpmp/slides/SlideControllerTest.java
+++ src/test/java/org/kpmp/slides/SlideControllerTest.java
@@ -14,6 +14,8 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.kpmp.logging.LoggingService;
+import org.kpmp.shibboleth.ShibbolethUser;
+import org.kpmp.shibboleth.ShibbolethUserService;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
@@ -24,11 +26,13 @@ public class SlideControllerTest {
 	private SlideController controller;
 	@Mock
 	private LoggingService logger;
+	@Mock
+	private ShibbolethUserService shibbolethUserService;
 
 	@Before
 	public void setUp() throws Exception {
 		MockitoAnnotations.initMocks(this);
-		controller = new SlideController(slideService, logger);
+		controller = new SlideController(slideService, logger, shibbolethUserService);
 	}
 
 	@After
@@ -58,4 +62,17 @@ public class SlideControllerTest {
 		assertEquals(slides, result);
 	}
 
+	@Test
+	public void testGetSlidesForParticipant() throws Exception {
+		HttpServletRequest request = mock(HttpServletRequest.class);
+		ShibbolethUser user = mock(ShibbolethUser.class);
+		when(user.getKpmpId()).thenReturn("123");
+		when(shibbolethUserService.getUser(request)).thenReturn(user);
+		List<Slide> slides = Arrays.asList(mock(Slide.class));
+		when(slideService.getSlidesForParticipant("123")).thenReturn(slides);
+
+		List<Slide> returnedSlides = controller.getSlidesForParticipant(request);
+
+		assertEquals(returnedSlides, slides);
+	}
 }
